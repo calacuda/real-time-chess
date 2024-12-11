51cfb28ecb998cdd4464252f93cb08491152c97d
Eoghan West <eowest@gmail.com>
2024-12-10 21:01:08 -0500

    backup commit

--- src/bin/rt-chess.rs
+++ src/bin/rt-chess.rs
@@ -135,9 +135,11 @@ fn update_visulizer_system(
     keyboard_input: Res<ButtonInput<KeyCode>>,
 ) {
     visualizer.add_network_info(client.network_info());
+
     if keyboard_input.just_pressed(KeyCode::F1) {
         *show_visualizer = !*show_visualizer;
     }
+
     if *show_visualizer {
         visualizer.show_window(egui_contexts.ctx_mut());
     }
